f5f8e8dbff13783d219ac006b186871f063b7bc6
FreddyR03 <colcreo@gmail.com>
2025-10-04 11:49:16 -0500

    se solucionaron errores

--- calculos.py
+++ calculos.py
@@ -17,24 +17,39 @@ def porcentaje_de_propina():
 
     if opcion == 1:
         monto=int(input("Ingrese el monto total de la cuenta: "))
+        if monto == 0:
+            print("No se puede cobrar un monto de 0$")
+            return
         porcentaje=(monto*10)/100
         total_pagar=print(f"el total del monto  ${monto} mas la propina ${porcentaje} es de ${monto+porcentaje}")
 
     elif opcion == 2:
         monto=float(input("Ingrese el monto total de la cuenta: "))
+        if monto == 0:
+            print("No se puede cobrar un monto de 0$")
+            return
         porcentaje=(monto*15)/100
         total_pagar=print(f"el total del monto  ${monto} mas la propina ${porcentaje} es de ${monto+porcentaje}")
 
 
     elif opcion == 3:
         monto=float(input("Ingrese el monto total de la cuenta: "))
+        if monto == 0:
+            print("No se puede cobrar un monto de 0$")
+            return
         porcentaje=(monto*20)/100
         total_pagar=print(f"el total del monto  ${monto} mas la propina ${porcentaje} es de ${monto+porcentaje}")
 
 
     elif opcion == 4:
         monto=float(input("Ingrese el monto total de la cuenta: "))
+        if monto == 0:
+            print("No se puede cobrar un monto de 0$")
+            return
         porcentaje=float(input("Ingrese el porcentaje que quieres dar de propina: "))
+        if porcentaje == 0:
+            print("No se puede dar una propina de 0%")
+            return
         resultado=(monto*porcentaje)/100
         total_pagar=print(f"el total del monto  ${monto} mas la propina ${resultado} es de ${monto+resultado}")
 
@@ -59,30 +74,57 @@ def porcentaje_de_propina_varias_personas():
 
     if opcion == 1:
         monto=int(input("Ingrese el monto total de la cuenta: "))
+        if monto == 0:
+            print("No se puede cobrar un monto de 0$")
+            return
         personas=int(input("Ingrese cuantas personas van a pagar la propina: "))
+        if personas == 0:
+            print("no puede haber 0 personas pagando la propina")
+            return
         porcentaje=(monto*10)/100
-        total_pagar=print(f"el total del monto ${monto} mas la propina ${porcentaje} que tiene que pagar las {personas} personas es de {(monto+porcentaje)/personas}")
+        total_pagar=print(f"el total del monto ${monto} mas la propina ${porcentaje} que tiene que pagar las {personas} personas es de {(monto+porcentaje)/personas} cada una.")
 
     elif opcion == 2:
         monto=float(input("Ingrese el monto total de la cuenta: "))
+        if monto == 0:
+            print("No se puede cobrar un monto de 0$")
+            return
         personas=int(input("Ingrese cuantas personas van a pagar la propina: "))
+        if personas == 0:
+            print("no puede haber 0 personas pagando la propina")
+            return
         porcentaje=(monto*15)/100
-        total_pagar=print(f"el total del monto ${monto} mas la propina ${porcentaje} que tiene que pagar las {personas} personas es de {(monto+porcentaje)/personas}")
+        total_pagar=print(f"el total del monto ${monto} mas la propina ${porcentaje} que tiene que pagar las {personas} personas es de {(monto+porcentaje)/personas} cada una.")
 
 
     elif opcion == 3:
         monto=float(input("Ingrese el monto total de la cuenta: "))
+        if monto == 0:
+            print("No se puede cobrar un monto de 0$")
+            return
         personas=int(input("Ingrese cuantas personas van a pagar la propina: "))
+        if personas == 0:
+            print("no puede haber 0 personas pagando la propina")
+            return
         porcentaje=(monto*20)/100
-        total_pagar=print(f"el total del monto ${monto} mas la propina ${porcentaje} que tiene que pagar las {personas} personas es de {(monto+porcentaje)/personas}")
+        total_pagar=print(f"el total del monto ${monto} mas la propina ${porcentaje} que tiene que pagar las {personas} personas es de {(monto+porcentaje)/personas} cada una.")
 
 
     elif opcion == 4:
         monto=float(input("Ingrese el monto total de la cuenta: "))
+        if monto == 0:
+            print("No se puede cobrar un monto de 0$")
+            return
         personas=int(input("Ingrese cuantas personas van a pagar la propina: "))
+        if personas == 0:
+            print("no puede haber 0 personas pagando la propina")
+            return
         porcentaje=float(input("Ingrese el porcentaje que quieres dar de propina: "))
+        if porcentaje == 0:
+            print("No se puede dar una propina de 0%")
+            return
         resultado=(monto*porcentaje)/100
-        total_pagar=print(f"el total del monto ${monto} mas la propina ${porcentaje} que tiene que pagar las {personas} personas es de {(monto+porcentaje)/personas}")
+        total_pagar=print(f"el total del monto ${monto} mas la propina ${porcentaje} que tiene que pagar las {personas} personas es de {(monto+porcentaje)/personas} cada una.")
 
     else:
         print("Ingresaste la opcion incorrecta")
